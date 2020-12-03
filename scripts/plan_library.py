7fdf886471bbb4445a99c5d3fc9abad23064cec1
ionut94 <ionut.moraru94@gmail.com>
2020-12-03 23:09:00 +0000

    made unique id for plans in plan lib + refractoring

--- scripts/plan_library.py
+++ scripts/plan_library.py
@@ -5,7 +5,9 @@ from std_msgs.msg import String
 from rosplan_knowledge_msgs.srv import GetDomainNameService, GetDomainTypeService, GetDomainOperatorService, \
     GetDomainAttributeService
 import yaml
+import uuid
 import pprint
+
 pp = pprint.PrettyPrinter(indent=1)
 
 
@@ -25,8 +27,8 @@ class PlanLibrary:
         self.problem_dictionary = {}
 
         # Get the domain from plan lib or initialise it from KB
-        if "domain" not in self.plan_dictionary.keys():
-            self.plan_dictionary["domain"] = self.get_domain_information_from_KB()
+        if self.plan_dictionary is None:
+            self.plan_dictionary = {"domain": self.get_domain_from_KB()}
         self.domain = self.plan_dictionary["domain"]
 
         # Create publishers to planner and plan_parser
@@ -37,8 +39,101 @@ class PlanLibrary:
         rospy.Subscriber(rospy.get_param("~problem_instance"), String, self.problem_callback)
         rospy.Subscriber(rospy.get_param("~planner_output"), String, self.planner_callback)
 
+    # Receive plan and save it into the planLib object
+    # TODO: Change the name for the entry in the plan library
+    def planner_callback(self, data):
+        self.latest_plan = data.data
+        self.latest_plan_time = rospy.Time.now()
+
+        self.problem_dictionary["current"].update({"plan": self.latest_plan})
+        self.plan_dictionary[str(uuid.uuid1().node)] = self.problem_dictionary["current"]
+
+        self.save_plan_library()
+        self.plan_lib_output_pub.publish(self.latest_plan)
+
+    # Receive problem and save it into the planLib object
+    def problem_callback(self, data):
+        self.latest_problem = data.data
+        self.latest_problem_time = rospy.Time.now()
+
+        self.problem_dictionary["current"] = self.parse_problem_to_dict(self.latest_problem)
+
+        check, plans = self.check_for_plan_in_library()
+        if check:
+            # self.latest_problem
+            rospy.loginfo("KCL: (%s) There is a plan for this problem in the plan library" % self.node_name)
+            self.plan_lib_output_pub.publish(plans[0])
+        else:
+            rospy.loginfo("KCL: (%s) Problem not in the plan library" % self.node_name)
+            self.planner_input_pub.publish(self.latest_problem)
+
+    # Check if the latest problem is already in our plan library
+    # TODO Implement a more robust version of searching for a plan in the PlanLib
+    def check_for_plan_in_library(self):
+        count = 0
+        plans = []
+
+        for problem_key, problem_data in self.plan_dictionary.iteritems():
+            if problem_key not in "domain":
+                if problem_data['problem'] in self.latest_problem.replace("\n", ""):
+                    count += 1
+                    plans.append(problem_data['plan'])
+                else:
+                    # TODO Check for plan possible to be modified from existing plan.
+                    pass
+
+        if count > 0:
+            return True, plans
+        else:
+            return False, plans
+
+    # take the problem file and parse it into the planLib object format needed
+    def parse_problem_to_dict(self, string_to_trim):
+        status = [False, False, False]
+        object_dict = {}
+        init_dict = {}
+        goal_dict = {}
+
+        for line in string_to_trim.split("\n"):
+            if ":objects" in line:
+                status = [True, False, False]
+            elif ":init" in line:
+                status = [False, True, False]
+            elif ":goal" in line:
+                status = [False, False, True]
+            elif (status[0] or status[1] or status[2]) and len(line.replace("(", "").replace(")", "")) > 0:
+                if status[0]:
+                    object_dict[line.split("-")[1]] = filter(None, line.split("-")[0].split(" "))
+                elif status[1]:
+                    self.process_line_for_parsing(init_dict, line)
+                elif status[2]:
+                    self.process_line_for_parsing(goal_dict, line)
+
+        problem_dict = {"problem": string_to_trim.replace("\n", ""), "object": object_dict, "init": init_dict,
+                        "goal": goal_dict}
+
+        return problem_dict
+
+    # Parse each line from init and goal of the problem
+    @staticmethod
+    def process_line_for_parsing(param_dict, line):
+        split_line = filter(None, line.replace("(", "").replace(")", "").split(" "))
+
+        # TODO: change this to see if functions are in the domain
+        if split_line[0] in "><=":
+            if split_line[1] in param_dict.keys():
+                param_dict[split_line[1]].append([split_line[0]] + split_line[2:])
+            else:
+                param_dict[split_line[1]] = [[split_line[0]] + split_line[2:]]
+
+        else:
+            if split_line[0] in param_dict.keys():
+                param_dict[split_line[0]].append(split_line[1:])
+            else:
+                param_dict[split_line[0]] = [split_line[1:]]
+
     # Get domain from the KB via service calls
-    def get_domain_information_from_KB(self):
+    def get_domain_from_KB(self):
         rospy.loginfo("KCL: (%s) Getting domain info from KB" % self.node_name)
 
         # The service names
@@ -106,95 +201,6 @@ class PlanLibrary:
 
         return domain_dict
 
-
-    # Receive plan and save it into the planLib object
-    # TODO: Change the name for the entry in the plan library
-    def planner_callback(self, data):
-        self.latest_plan = data.data
-        self.latest_plan_time = rospy.Time.now()
-
-        self.problem_dictionary["test"].update({"plan": self.latest_plan})
-        self.plan_dictionary["test"] = self.problem_dictionary["test"]
-
-        self.save_plan_library()
-        self.plan_lib_output_pub.publish(self.latest_plan)
-
-    # Receive problem and save it into the planLib object
-    def problem_callback(self, data):
-        self.latest_problem = data.data
-        self.latest_problem_time = rospy.Time.now()
-
-        self.problem_dictionary["test"] = self.parse_problem_to_dict(self.latest_problem)
-
-        check, plans = self.check_for_plan_in_library()
-        if check:
-            # self.latest_problem
-            rospy.loginfo("KCL: (%s) There is a plan for this problem in the plan library" % self.node_name)
-            self.plan_lib_output_pub.publish(plans[0])
-        else:
-            rospy.loginfo("KCL: (%s) Problem not in the plan library" % self.node_name)
-            self.planner_input_pub.publish(self.latest_problem)
-
-    # Check if the latest problem is already in our plan library
-    # TODO Implement a more robust version of searching for a plan in the PlanLib
-    def check_for_plan_in_library(self):
-        count = 0
-        plans = []
-
-        for problem, data in self.plan_dictionary.iteritems():
-            if data['problem'] in self.latest_problem.replace("\n", ""):
-                count += 1
-                plans.append(data['plan'])
-
-        if count > 0:
-            return True, plans
-        else:
-            return False, plans
-
-    # take the problem file and parse it into the planLib object format needed
-    def parse_problem_to_dict(self, string_to_trim):
-        status = [False, False, False]
-        object_dict = {}
-        init_dict = {}
-        goal_dict = {}
-
-        for line in string_to_trim.split("\n"):
-            if ":objects" in line:
-                status = [True, False, False]
-            elif ":init" in line:
-                status = [False, True, False]
-            elif ":goal" in line:
-                status = [False, False, True]
-            elif (status[0] or status[1] or status[2]) and len(line.replace("(", "").replace(")", "")) > 0:
-
-                if status[0]:
-                    object_dict[line.split("-")[1]] = filter(None, line.split("-")[0].split(" "))
-                elif status[1]:
-                    self.process_line_for_parsing(init_dict, line)
-                elif status[2]:
-                    self.process_line_for_parsing(goal_dict, line)
-
-        problem_dict = {"problem": string_to_trim.replace("\n", ""), "object": object_dict, "init": init_dict,
-                        "goal": goal_dict}
-
-        return problem_dict
-
-    # Parse each line from init and goal of the problem
-    @staticmethod
-    def process_line_for_parsing(line_dict, line):
-        line_split = filter(None, line.replace("(", "").replace(")", "").split(" "))
-        if line_split[0] in "><=":
-            if line_split[1] in line_dict.keys():
-                line_dict[line_split[1]].append([line_split[0]] + line_split[2:])
-            else:
-                line_dict[line_split[1]] = [[line_split[0]] + line_split[2:]]
-
-        else:
-            if line_split[0] in line_dict.keys():
-                line_dict[line_split[0]].append(line_split[1:])
-            else:
-                line_dict[line_split[0]] = [line_split[1:]]
-
     # Take the current plans from the planLib object and parse them back into the yaml format.
     def save_plan_library(self):
         stream = file(self.plans_path, 'w')
@@ -208,8 +214,6 @@ class PlanLibrary:
             loaded_plan_lib = yaml.load(plan_lib, Loader=yaml.FullLoader)
 
         return loaded_plan_lib
-
-
 
 
 if __name__ == "__main__":
