1bb73b5e32f628aeb3710b20587b9a8684a739c1
Moraru <k1333921@pddlplus.nms.kcl.ac.uk>
2020-12-14 14:37:58 +0000

    changed service call wait to 30

--- experiments/run_experiment.sh
+++ experiments/run_experiment.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 # This is set to be run inside the docker container!
 
 echo "RUNNING PLAN LIBRARY EXPERIMENT"
@@ -10,20 +10,20 @@ source devel/setup.bash
 PROBLEMS_PATH="/root/ws/src/plan_library/common/benchmarks/office"
 
 for INSTANCE in $(ls $PROBLEMS_PATH/problem_*); do
-    sleep 7 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
+    sleep 30 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
     timeout 500 roslaunch plan_library test_plan_lib.launch $@ problem_path:=$INSTANCE
     echo -e '\n\n\n\n\n'
 done
 
 for INSTANCE in $(ls $PROBLEMS_PATH/problem_*); do
-    sleep 7 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
+    sleep 30 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
     timeout 500 roslaunch plan_library test_plan_lib.launch $@ problem_path:=$INSTANCE
     echo -e '\n\n\n\n\n'
 done
 
 
 for INSTANCE in $(ls $PROBLEMS_PATH/problem_*); do
-    sleep 7 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
+    sleep 30 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
     timeout 500 roslaunch plan_library test_plan_lib.launch $@ problem_path:=$INSTANCE
     echo -e '\n\n\n\n\n'
 done
