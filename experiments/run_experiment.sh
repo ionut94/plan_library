f5cf7f54a58bf62a36a24f20f545fd265bb2de0c
ionut94 <ionut.moraru94@gmail.com>
2020-12-14 12:39:43 +0000

    Changed experiments

--- experiments/run_experiment.sh
+++ experiments/run_experiment.sh
@@ -7,6 +7,23 @@ echo -e "PARAMS: $@ \n\n"
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
+PROBLEMS_PATH="/root/ws/src/plan_library/common/benchmarks/office"
 
-roslaunch plan_library test_plan_lib.launch $@  # To set: object file and initial state
+for INSTANCE in $(ls $PROBLEMS_PATH/problem_*); do
+    sleep 7 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
+    timeout 500 roslaunch plan_library test_plan_lib.launch $@ problem_path:=$PROBLEMS_PATH/$INSTANCE
+    echo -e '\n\n\n\n\n'
+done
 
+for INSTANCE in $(ls $PROBLEMS_PATH/problem_*); do
+    sleep 7 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
+    timeout 500 roslaunch plan_library test_plan_lib.launch $@ problem_path:=$PROBLEMS_PATH/$INSTANCE
+    echo -e '\n\n\n\n\n'
+done
+
+
+for INSTANCE in $(ls $PROBLEMS_PATH/problem_*); do
+    sleep 7 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
+    timeout 500 roslaunch plan_library test_plan_lib.launch $@ problem_path:=$PROBLEMS_PATH/$INSTANCE
+    echo -e '\n\n\n\n\n'
+done
