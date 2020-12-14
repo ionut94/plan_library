f40e3b633eb3219e2bc640eced4b49d497dab6e0
ionut94 <ionut.moraru94@gmail.com>
2020-12-14 23:16:35 +0000

    removed unused var

--- experiments/run_experiment.sh
+++ experiments/run_experiment.sh
@@ -7,8 +7,6 @@ echo -e "PARAMS: $@ \n\n"
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
-PROBLEMS_PATH="/root/ws/src/plan_library/common/benchmarks/office"
-
 for i in {1..20}; do
     sleep 30 && rosservice call /plan_library/execute_current_problem & # Wait for the nodes to launch, then trigger planning
     timeout 500 roslaunch plan_library test_plan_lib.launch $@
